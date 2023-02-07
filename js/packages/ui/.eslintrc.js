cf8a236b1a728288be62f7098190d76ed7e39f1a
Jon <jon@jonrose.dev>
2023-02-06 23:06:37 -0500

    Fix linting

--- js/packages/ui/.eslintrc.js
+++ js/packages/ui/.eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
   root: true,
   extends: ['eslint-config-custom-react'],
-  ignorePatterns: ['next.config.js'],
   parserOptions: {
-    project: './apps/docs/tsconfig.json',
+    project: './packages/ui/tsconfig.json',
   },
 };
--- js/packages/ui/index.tsx
+++ js/packages/ui/index.tsx
@@ -1,2 +1,1 @@
-import * as React from "react";
-export * from "./Button";
+export { Button, type ButtonProps } from './button';
