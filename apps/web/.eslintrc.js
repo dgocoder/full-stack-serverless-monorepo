acbd3183d00b38157903ce25b9959bc12a9fe4be
Jon <jon@jonrose.dev>
2023-02-13 21:05:38 -0500

    Fix build

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   extends: ['eslint-config-custom-react'],
   ignorePatterns: ['next.config.js'],
   parserOptions: {
-    project: './apps/web/tsconfig.json',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
   },
 };
